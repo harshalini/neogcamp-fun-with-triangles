07f70f64955ec7fc36c78f4f7aa81751ce6e38e5
harshalini <harshalinipandhare29@gmail.com>
2021-08-21 16:08:50 +0530

    added quiz questions

--- quiz.js
+++ quiz.js
@@ -1,20 +1,23 @@
 const quizForm = document.querySelector(".quiz-form");
 const submitButton = document.querySelector("#submit-button");
+const questions = document.querySelectorAll(".question-container");
 const output = document.querySelector("#output");
 
-const correctAns = ["75", "right angled", "obtuse", "acute", "Isosceles", "Equilateral"];
+const correctAns = ["75", "right angled", "obtuse", "acute", "Isosceles", "Equilateral", "3*a", "40"];
 
-function calculateScore() {
-    let score = 0;
-    let index = 0;
+let score = 0;
+quizForm.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
     const formResults = new FormData(quizForm);
-
-    for (let value of formResults.values()) {
-        if (value === correctAns[index]) {
+    let index = 0;
+    for (let value of formResults) {
+        if (value[1] == correctAns[index]) {
             score = score + 1;
         }
         index = index + 1;
     }
-    output.innerText = "Your score is :" + score;
-};
-submitButton.addEventListener("click", calculateScore);
+    output.innerText = "Your score is: " + score;
+    submitButton.style.display = "none";
+});
